d701db0f5ff5d826fa48e55d8b12a98bb75e25fd
chriskrunchy <guesswho2319@gmail.com>
2023-11-04 21:45:08 -0400

    added files

--- Random Forest REgressor.py
+++ Random Forest REgressor.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-import panadas as pd
+import pandas as pd
 
 df = pd.read_csv('file_name.csv')
 
@@ -16,8 +16,7 @@ for i in df.columns:
 X = df.drop('price', axis = 1)  # Feature matrix
 y = df['price']    # Target variable
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create a Random Forest Regressor model
 rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
